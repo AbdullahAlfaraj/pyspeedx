c8ba78314399794c83bc1e257a753c4037338013
AbdullahAlfaraj <abdullah.a.f2014@gmail.com>
2020-09-09 09:12:30 +0300

    First commit

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from pyspeedx.Test import Test
---
+++ test.py
@@ -0,0 +1,10 @@
+class Test:
+    def print_hello():
+        print("hey there! wellcome to PySpeedX")
+
+
+
+
+if __name__ == "__main__":
+
+    Test.print_hello()
